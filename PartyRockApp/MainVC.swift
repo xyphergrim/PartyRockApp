d5be8318944e6de57b6289e0d4024abf633dce0f
xyphergrim <johnclancy@outlook.com>
2016-11-17 13:17:03 -0600

    initial ui feature branch commit
    building out the ui for party rock app

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by John on 11/17/16.
+//  Copyright © 2016 jpclancy. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
