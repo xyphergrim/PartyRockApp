025ada126542cfdb45b44dfc14fee9d23a803532
xyphergrim <johnclancy@outlook.com>
2016-11-17 13:52:55 -0600

    setup some code logic for mvc

--- PartyRockApp/MainVC.swift
+++ PartyRockApp/MainVC.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var partyRocks = [PartyRock]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
 
 }
