f2b42e7e72a205742b98aaf11ba0df44e000502b
xyphergrim <johnclancy@outlook.com>
2016-11-17 15:19:31 -0600

    added table view cell code
    hard coding data to present into the table view cell - implementing the
    functions

--- PartyRockApp/MainVC.swift
+++ PartyRockApp/MainVC.swift
@@ -17,14 +17,30 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let p1 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ev4bY1SkZLg\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Where The Sun Goes")
+        
+        //let urlTest = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ev4bY1SkZLg\" frameborder=\"0\" allowfullscreen></iframe>"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PartyCell", for: indexPath) as? PartyCell {
+            let partyRock = partyRocks[indexPath.row]
+            
+            cell.updateUI(partyRock: partyRock)
+            
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return partyRocks.count
     }
 
 }
